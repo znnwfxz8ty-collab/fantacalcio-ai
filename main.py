e688a42ea4f12459ff7dc15c492e799ef2e6af0e
znnwfxz8ty-collab <znnwfxz8ty@privaterelay.appleid.com>
2026-01-21 19:50:37 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def run():
+    team = [
+        "Svilar",
+        "Bremer", "Scalvini", "Mancini", "Dodò",
+        "Koopmeiners", "Ederson", "Zaccagni", "Soulé",
+        "Nkunku", "Kean"
+    ]
+
+    score = 78.5
+
+    return {
+        "team": team,
+        "score": score
+    }
